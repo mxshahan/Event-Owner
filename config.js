0ae6c62dd488fd4739a19f2d7fe75fccf09e702a
Misbauddin Chowdhury Shahan <shahan953@hotmail.com>
2018-08-15 11:44:39 +0600

    cep

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const api = {
     server: 'https://event-owner.herokuapp.com',
-    dev: 'http://127.0.0.1:3000'
+    dev: 'http://127.0.0.1:3000',
+    siteurl: 'https://event-owner.herokuapp.com'
 }
 
 module.exports = api;
