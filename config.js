54762342b87e8d990681107ec6cf1267840591b7
Misbauddin Chowdhury Shahan <shahan953@hotmail.com>
2018-09-12 02:24:04 +0600

    spl

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const api = {
-    server: process.env.NODE_ENV === 'production' ? 'https://event-owner.herokuapp.com' : 'http://127.0.0.1:3000',
+    server: process.env.NODE_ENV === 'production' ? 'https://event-owner.herokuapp.com' | 'http://localhost:3000' : 'http://127.0.0.1:3000',
     dev: 'http://127.0.0.1:3000',
     siteurl: 'http://127.0.0.1:3000'
 }
