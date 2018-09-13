79c1cf6a8b5ababc8ba6685a3dcef0fa46837390
Misbauddin Chowdhury Shahan <shahan953@hotmail.com>
2018-09-13 15:00:52 +0600

    deleted

--- config.js
+++
@@ -1,7 +0,0 @@
-const api = {
-    server: process.env.NODE_ENV === 'production' ? 'https://event-owner.herokuapp.com' : 'http://127.0.0.1:3000',
-    dev: 'http://127.0.0.1:3000',
-    siteurl: 'http://127.0.0.1:3000'
-}
-
-export default api;
