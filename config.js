4e7f3d68bad21765d6d80a0c4a6779d9b7bb5da6
Misbauddin Chowdhury Shahan <shahan953@hotmail.com>
2018-09-25 19:31:09 +0600

    admin init

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 const api = {
-    server: process.env.NODE_ENV.includes('production') ? '' : 'http://127.0.0.1:3000',
+    server: process.env.NODE_ENV === 'production' ? '' : 'http://127.0.0.1:3000',
     dev: 'http://127.0.0.1:3000',
-    siteurl: 'http://127.0.0.1:3000'
+    siteurl: 'http://127.0.0.1:3000',
+    events: {
+      type: ['Wedding', 'Bar/Bat', 'Bris', 'Birthday', 'Other']
+    }
 }
 
 export default api;
